183007927eae58b3e758cfecadb64352ac40f0cc
Priyankateke <priiyankateke1705@gmail.com>
2020-05-14 01:01:22 +0530

    ADDED: Random Function To Check Gambler Win Or Lose

--- gamblingSimulator.js
+++ gamblingSimulator.js
@@ -1,5 +1,25 @@
 console.log("Welcome To Gambling Simulator")
 
-//variables
+//constants
 const EVERY_DAY_STAKE=100
 const BET=1
+
+//variables
+cash=EVERY_DAY_STAKE
+
+var RandomValue = Math.random()
+
+//Function to check win or loss 
+function dailyBet()
+{
+	if (RandomValue < 0.5 ) {
+		console.log("Win")
+        cash += BET
+    }
+	else {
+		console.log("Lose")
+        cash -= BET
+    }
+}
+
+dailyBet()
