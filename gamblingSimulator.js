f4f094403a477e7c01f573aaa3a579e73d35e267
Priyankateke <priiyankateke1705@gmail.com>
2020-05-14 09:21:48 +0530

    ADDED: Calculated Total Amount Of Win/Lose Of 20 Days

--- gamblingSimulator.js
+++ gamblingSimulator.js
@@ -1,17 +1,24 @@
 console.log("Welcome To Gambling Simulator")
 
 //constants
-const EVERY_DAY_STAKE=100
-const BET=1 
+const EVERY_DAY_STAKE = 100
+const BET = 1 
+const TOTAL_DAYS = 20
 const LOWER_PERCENT = EVERY_DAY_STAKE - (EVERY_DAY_STAKE * 50 /100)
 const UPPER_PERCENT = EVERY_DAY_STAKE + (EVERY_DAY_STAKE * 50 /100)
 
 //variables
-cash=EVERY_DAY_STAKE
+var cash
+var totalAmount = 0
+var gainCash
+
+var dict = {};
 
 //Function to check win or loss 
 function dailyBet()
 {
+    gainCash=0
+    cash = EVERY_DAY_STAKE
     while( cash > LOWER_PERCENT && cash < UPPER_PERCENT ) {
         var RandomValue = Math.random()
         if (RandomValue < 0.5 ) {
@@ -21,6 +28,26 @@ function dailyBet()
             cash -= BET
         }
     }
+    gainCash = cash - EVERY_DAY_STAKE
+    return gainCash
 }
 
-dailyBet()
+//function for Monthly Bet
+function monthlyGambling() {
+    for( day = 1; day <= TOTAL_DAYS; day++ ) {
+        var cash = dailyBet()
+        //storing Each Day amount in Dictionary
+        dict["Day "+day] = cash
+        totalAmount = totalAmount + dict["Day "+day]
+        console.log("Day "+day+" = "+dict["Day "+day])
+    }
+
+    if( totalAmount > 0 ) {
+        console.log("Total Amount Won In 20 Days "+totalAmount)
+    }
+    else {
+        console.log("Total Amount Loose in 20 Days "+totalAmount)
+    }
+}
+
+monthlyGambling()
